e62638f9c53038987f0871e88a4f26ce28bc1d71
Shiphu <psphu2003@gmail.com>
2023-12-15 18:33:12 +0700

    updated UI of each user in system

--- N08_CSDLNC_QUANLYPHONGKHAMNHAKHOA/N08_CSDLNC_QUANLYPHONGKHAMNHAKHOA/Program.cs
+++ N08_CSDLNC_QUANLYPHONGKHAMNHAKHOA/N08_CSDLNC_QUANLYPHONGKHAMNHAKHOA/Program.cs
@@ -16,7 +16,7 @@ namespace N08_CSDLNC_QUANLYPHONGKHAMNHAKHOA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SIGN_IN());
         }
     }
 }
--- N08_CSDLNC_QUANLYPHONGKHAMNHAKHOA/N08_CSDLNC_QUANLYPHONGKHAMNHAKHOA/QUANTRIVIEN.cs
+++ N08_CSDLNC_QUANLYPHONGKHAMNHAKHOA/N08_CSDLNC_QUANLYPHONGKHAMNHAKHOA/QUANTRIVIEN.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace N08_CSDLNC_QUANLYPHONGKHAMNHAKHOA
 {
-    public partial class Form1 : Form
+    public partial class QUANTRIVIEN : Form
     {
-        public Form1()
+        public QUANTRIVIEN()
         {
             InitializeComponent();
         }
