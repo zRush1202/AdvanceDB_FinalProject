7aa188284dea04932044a0d82b3ffcf163f67b47
Shiphu <psphu2003@gmail.com>
2023-12-15 18:09:37 +0700

    add UI_Code

---
+++ UI_Code/NHOM04_UI_PHONGKHAMNHAKHOA/NHOM04_UI_PHONGKHAMNHAKHOA/Program.cs
@@ -0,0 +1,17 @@
+namespace NHOM04_UI_PHONGKHAMNHAKHOA
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new SIGN_IN());
+        }
+    }
+}
---
+++ UI_Code/NHOM04_UI_PHONGKHAMNHAKHOA/NHOM04_UI_PHONGKHAMNHAKHOA/SIGN_IN.cs
@@ -0,0 +1,12 @@
+namespace NHOM04_UI_PHONGKHAMNHAKHOA
+{
+    public partial class SIGN_IN : Form
+    {
+        public SIGN_IN()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
