3858458eeea8e34f219b25a40f8b2c31698619bd
Shiphu <psphu2003@gmail.com>
2023-12-17 16:36:01 +0700

    ad ico for DATLICHHEN

--- N08_CSDLNC_QUANLYPHONGKHAMNHAKHOA/N08_CSDLNC_QUANLYPHONGKHAMNHAKHOA/DATLICHHEN.Designer.cs
+++ N08_CSDLNC_QUANLYPHONGKHAMNHAKHOA/N08_CSDLNC_QUANLYPHONGKHAMNHAKHOA/DATLICHHEN.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DATLICHHEN));
             this.lb_phongkham = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -335,6 +336,7 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel_cyan);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "DATLICHHEN";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DATLICHHEN";
